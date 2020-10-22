bb25a6bf54406575071178b0e26bb34f58b3745a
Ruben Cedeno <rubencedeno@rubens-mbp.myfiosgateway.com>
2020-10-22 09:56:36 -0400

    [hello]Add sayHello function

---
+++ hello.js
@@ -0,0 +1,31 @@
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+console.log(sayHello("SEI 1019"));
+
+function sayHello(name) {
+    let msg = 'Hello ' + name + '. How are you?';
+    return msg;
+}
+console.log(sayHello("Ruben"));
+
+function sayHello(name) {
+    let msg = 'Hello ' + name + '. How are you?';
+    return msg;
+}
+console.log(sayHello("John"));
+
+function sayHello(name) {
+    let msg = 'Hello ' + name + '. How are you?';
+    return msg;
+}
+console.log(sayHello("Jason"));
+
+function sayHello(name) {
+    let msg = 'Hello ' + name + '. How are you?';
+    return msg;
+}
+console.log(sayHello('Eric'));
+
+function checkNumber(num) {}
