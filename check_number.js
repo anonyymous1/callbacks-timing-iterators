6d7e67705750c74a9c23c7698d69880bdceefacd
Ruben Cedeno <rubencedeno@rubens-mbp.myfiosgateway.com>
2020-10-22 10:15:24 -0400

    [check]Add checkNumber functions

---
+++ check_number.js
@@ -0,0 +1,55 @@
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+
+console.log(checkNumber(5));
+
+function checkNumber (num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num > 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+console.log(checkNumber(8));
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'postive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+console.log(checkNumber(0));
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+  }
+}
+console.log(checkNumber(-2));
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive number';
+    } else if (num < 0) {
+        return 'negative number';
+    } else {
+        return 'it was zero';
+    }
+}
+console.log(checkNumber(-4));
